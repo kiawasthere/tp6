12648ad2d6615b1969c42c4afce04af467e37246
Kia <pruittzakiya@gmail.com>
2022-10-13 04:36:10 +0000

    updated with AJAS calls and timeout function

--- scripts.js
+++ scripts.js
@@ -1,6 +1,44 @@
-// Javascript for TP5 
+// Javascript for TP6
+//function to load file from the URL "fromFile" into the object identified by "whereTo"
+function loadFileInto(fromFile, whereTo) {
+
+	// creating a new XMLHttpRequest object
+	ajax = new XMLHttpRequest();
+
+	// defines the GET/POST method, source, and async value of the AJAX object
+	ajax.open("GET", fromFile, true);
+
+	// provides code to do something in response to the AJAX request
+	ajax.onreadystatechange = function() {
+			if ((this.readyState == 4) && (this.status == 200)) {
+				document.querySelector(whereTo).innerHTML += this.responseText;
+				
+			} else if ((this.readyState == 4) && (this.status != 200)) {
+				console.log("Error: " + this.responseText);
+			}
+		
+	} // end ajax.onreadystatechange function
+
+	// initiate request and wait for response
+	ajax.send();
+
+}
+
+
+
+
+
+
+
+
 
 window.onload = function() {
+  
+    
+loadFileInto("ingredients.html", "#ingredients ul");    
+loadFileInto("equipment.html", "#equipment ul");
+loadFileInto("directions.html", "#directions ol");
+  
   
   document.querySelector("#ingredients h4").classList.add("tp");
   
@@ -25,9 +63,13 @@ window.onload = function() {
     this.classList.toggle("tpShow");
     
   }
-    
-    
+    setTimeout(function() {
+          
    document.querySelector("#row #ingredients ul").innerHTML += "<li> A hunger for chocolate </li>";
+      
+    }, 100);
+
+  
 
 
   
